4ae4ee8fbd42f295ef236dd37ade63f3ad87e526
Hecan7 <7hecano@gmail.com>
2020-05-15 17:46:41 -0700

    first commit

--- Public/assets/js/main.js
+++ Public/assets/js/main.js
@@ -1,34 +1,20 @@
-var projects = document.getElementById("projects");
 
-document.addEventListener("DOMContentLoaded", function () {
-	if (localStorage.getItem("mail") == "sent") {
-		alert("Message Sent: Thank you for your message.");
-		localStorage.setItem("mail","");
-	}
-});	
-
-projects.addEventListener("click", function(){
-	window.scrollTo({
-		top:1500,
-		bottom:0,
-		behavior: "smooth"
-	});
-});
-
-email.addEventListener("click", function(){
-	window.scrollTo({
-		top:4500,
-		bottom:0,
-		behavior: "smooth"
-	});
-});
-
-function sendEmail() {
-	var email = document.getElementById("email").value;
-	var name = document.getElementById("name").value;
-	var message = document.getElementById("message").value;
-	if(email != "" && name != "" && message != ""){
-	localStorage.setItem("mail","sent");
+function sendNewPass() {
+	console.log('function fired');
+	event.preventDefault();
+	var url = window.location;
+	var token = new URLSearchParams(url.search).get('access_token');
+	var userId = new URLSearchParams(url.search).get('userId');
+	var new_pass = document.getElementById("new_pass").value;
+	var confirm = document.getElementById("confirm").value;
+	console.log('token:', token, '-userId:', userId);
+	if(new_pass != "" && confirm != "" && new_pass == confirm){
+		axios.post(`http://192.168.1.34:3000/api/Customers/reset-password?access_token=${token}&userId=${userId}`, {
+			newPassword: new_pass
+		}).then((response) => {
+			console.log(response);
+		}, (error) => {
+			console.log(error);
+		});
 	}
 };
-
--- server.js
+++ server.js
@@ -1,37 +1,21 @@
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 5500;
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const app = express();
-const mailer = require("./Public/assets/js/mailer");
+const mailer = require("./public/assets/js/mailer");
 
 app.listen(PORT, () => {
-    console.log("listening at http://localhost:3000")
+    console.log("listening at http://localhost:5500")
 });
-app.use(express.static('Public'));
-
+app.use(express.static('public'));
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
-// app.set('views' , './views');
-
-// app.set('view engine' , 'ejs');
 
 app.get('/',(req, res) => {
+    console.log('a get to the /reset-password route was hit', req.query.access_token);
+    if (!req.query.access_token) return res.sendStatus(401);
     res.sendFile('index.html', {root: __dirname })
-});
-
-app.get('/contact', (req, res) => {
-res.render('contact');
-});
-
-app.post('/thanks', (req, res) => {
-res.render('thanks', { contact: req.body })
-});
-
-app.post('/submit', (req, res) => {
-    console.log(req.body);
-mailer(req);
-res.sendFile('index.html', {root: __dirname });
 });
